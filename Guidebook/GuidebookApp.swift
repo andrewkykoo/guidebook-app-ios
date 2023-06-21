b9da7df1e94b81834e6e7c0238d923e3d13c6731
Andrew Koo <andrewkykoo@gmail.com>
2023-06-21 18:11:59 -0400

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Andrew Koo on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
